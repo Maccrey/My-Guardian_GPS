e2ea084794a406a19fc0d10070df1f08df03ddfe
Maccrey <m01071630214@gmail.com>
2025-05-14 19:38:54 +0900

    Firebase 패키지 업데이트 및 iOS 설정 개선: pubspec.yaml에서 Firebase Core, Auth, Cloud Firestore의 버전을 낮추고, Podfile에서 Firestore 프리컴파일된 바이너리 사용 설정 추가. iOS 및 macOS에서 플러그인 등록 로직을 업데이트하여 플랫폼 간 호환성 향상.

--- lib/main.dart
+++ lib/main.dart
@@ -24,7 +24,7 @@ import 'views/map_view.dart';
 import 'views/settings/settings_view.dart';
 import 'services/settings_service.dart';
 
-import 'firebase_options.dart'; // 임시로 주석 처리
+import 'firebase_options.dart';
 
 // SharedPreferences 초기화 상태를 추적하는 플래그
 bool isSharedPreferencesAvailable = false;
@@ -38,7 +38,6 @@ void main() async {
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
-  // Firebase 초기화 코드 제거 - 테스트를 위해
 
   // 백그라운드 오디오 초기화
   try {
--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -7,6 +7,7 @@ import Foundation
 
 import audio_service
 import audio_session
+import cloud_firestore
 import firebase_auth
 import firebase_core
 import geolocator_apple
@@ -19,6 +20,7 @@ import url_launcher_macos
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   AudioServicePlugin.register(with: registry.registrar(forPlugin: "AudioServicePlugin"))
   AudioSessionPlugin.register(with: registry.registrar(forPlugin: "AudioSessionPlugin"))
+  FLTFirebaseFirestorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseFirestorePlugin"))
   FLTFirebaseAuthPlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseAuthPlugin"))
   FLTFirebaseCorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseCorePlugin"))
   GeolocatorPlugin.register(with: registry.registrar(forPlugin: "GeolocatorPlugin"))
