1797eb0b3e95783a92f67edb728a7262ca48902c
Maccrey <m01071630214@gmail.com>
2025-05-24 18:46:08 +0900

    pubspec.lock 및 pubspec.yaml에서 flutter_local_notifications 패키지를 제거하고, 관련 코드에서 해당 플러그인 등록을 삭제하였습니다. 서브프로젝트 커밋 상태를 'dirty'로 변경하였습니다.

--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -13,7 +13,6 @@ import file_selector_macos
 import firebase_auth
 import firebase_core
 import firebase_storage
-import flutter_local_notifications
 import geolocator_apple
 import google_sign_in_ios
 import just_audio
@@ -32,7 +31,6 @@ func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   FLTFirebaseAuthPlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseAuthPlugin"))
   FLTFirebaseCorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseCorePlugin"))
   FLTFirebaseStoragePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseStoragePlugin"))
-  FlutterLocalNotificationsPlugin.register(with: registry.registrar(forPlugin: "FlutterLocalNotificationsPlugin"))
   GeolocatorPlugin.register(with: registry.registrar(forPlugin: "GeolocatorPlugin"))
   FLTGoogleSignInPlugin.register(with: registry.registrar(forPlugin: "FLTGoogleSignInPlugin"))
   JustAudioPlugin.register(with: registry.registrar(forPlugin: "JustAudioPlugin"))
